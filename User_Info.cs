1103f778d5b0c73aca9da2520de9e23c4d3e6bdf
subasiseyda <subasiseyda96@gmail.com>
2018-10-04 22:25:19 +0300

    Add files via upload

---
+++ User_Info.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class User_Info
+    {
+        public static string userid { get; set; }
+        public static string username { get; set; }
+        public static string name { get; set; }
+        public static string email { get; set; }
+        public static string password { get; set; }
+        public static string phone { get; set; }
+        public static string type { get; set; }
+        public static DateTime startdate { get; set; }
+        public static DateTime enddate { get; set; }
+
+       
+    }
+  
+
+}
